83c21c9d3645574aa249e459bf62a030486a79fc
Elena Voinu <37947819+Lenka111@users.noreply.github.com>
2020-03-26 19:02:49 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,14 +4,19 @@ import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
+        //create set 1
         Set<Integer> set1 = new HashSet<>();
         set1.add(1);
         set1.add(3);
         set1.add(4);
+        //display set elements
         System.out.println("set1 =" + set1);
+        
+        //create set2
         Set<Integer>set2 = new HashSet<>();
         set2.add(3);
         set2.add(1);
+        
         System.out.println("set2 =" + set2);
         //union of two sets
         set1.addAll(set2);
