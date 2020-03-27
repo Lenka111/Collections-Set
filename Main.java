f7c6dd236e8f418318e9f966201c6672225c5ce7
Elena Voinu <37947819+Lenka111@users.noreply.github.com>
2020-03-26 19:01:43 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,27 @@
+// Elena Voinu
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Set<Integer> set1 = new HashSet<>();
+        set1.add(1);
+        set1.add(3);
+        set1.add(4);
+        System.out.println("set1 =" + set1);
+        Set<Integer>set2 = new HashSet<>();
+        set2.add(3);
+        set2.add(1);
+        System.out.println("set2 =" + set2);
+        //union of two sets
+        set1.addAll(set2);
+        System.out.println("union of the sets is: "+set1);
+
+        //intersection of the sets
+        set1.retainAll(set2);
+        System.out.println("intersection is: "+set1);
+
+
+
+    }
+}
